589129bc4d05c75325a44543709f29f24714e8c4
진찬호 <chanhojin94@kakao.com>
2023-12-16 15:07:42 +0900

    signup,signin

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var react_1 = __importDefault(require("react"));
-var App = function () {
-    return react_1.default.createElement("div", null, "Hello, TSX!");
-};
-exports.default = App;
+document.addEventListener("DOMContentLoaded", () => {
+  const signupForm = document.getElementById("signup");
+  const loginForm = document.getElementById("login");
+
+  signupForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const email = document.getElementById("signup-email").value;
+    const password = document.getElementById("signup-password").value;
+
+    // TODO: Send signup data to the backend
+    console.log("Signing up...", email, password);
+  });
+
+  loginForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const email = document.getElementById("login-email").value;
+    const password = document.getElementById("login-password").value;
+
+    // TODO: Send login data to the backend
+    console.log("Logging in...", email, password);
+  });
+});
